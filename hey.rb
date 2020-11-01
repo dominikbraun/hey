2aaf7d70eea833fdd3d4f8d6d5b160138cd00123
Dominik Braun <mail@dominikbraun.io>
2020-11-01 20:37:23 +0100

    Create hey web server

---
+++ hey.rb
@@ -0,0 +1,28 @@
+require 'socket'
+
+hey = <<-HEREDOC
+
+    __
+   / /_  ___  __  __
+  / __ \/ _ \/ / / /
+ / / / /  __/ /_/ /
+/_/ /_/\___/\__, /
+           /____/
+
+HEREDOC
+
+server = TCPServer.new 5678
+
+while session = server.accept
+  request = session.gets
+  puts request
+
+  session.print "HTTP/1.1 200\r\n" # 1
+  session.print "Content-Type: text/html\r\n" # 2
+  session.print "\r\n" # 3
+
+  session.print "<style>*{font-family:monospace}</style>"
+  session.print "<pre>#{hey}</pre>"
+
+  session.close
+end
